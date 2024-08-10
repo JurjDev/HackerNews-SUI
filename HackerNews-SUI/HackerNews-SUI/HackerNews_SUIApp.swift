22e7af703eb5712fc7ca2f83b020db05c0501172
JurjDev <jurjdev@gmail.com>
2024-08-10 17:08:20 -0500

    Initial commit

---
+++ HackerNews-SUI/HackerNews-SUI/HackerNews_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_SUIApp.swift
+//  HackerNews-SUI
+//
+//  Created by JurjDev on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
